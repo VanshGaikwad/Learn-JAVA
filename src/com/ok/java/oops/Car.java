815137ed3c7e88ba6965115d3f8a5227de6b5191
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-09-13 18:17:49 +0530

    OOPS

---
+++ src/com/ok/java/oops/Car.java
@@ -0,0 +1,12 @@
+package com.ok.java.oops;
+
+import java.lang.invoke.StringConcatFactory;
+
+public class Car {
+    int speed;
+    String color;
+
+    void drive(){
+        System.out.println("Driving....");
+    }
+}

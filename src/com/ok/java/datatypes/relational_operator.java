8f64bcb068502ea10d74ce3862003ca648009f08
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-09-13 17:33:51 +0530

    Relational operator, Logical Operator

---
+++ src/com/ok/java/datatypes/relational_operator.java
@@ -0,0 +1,26 @@
+package com.ok.java.datatypes;
+
+public class relational_operator {
+    public static void main(String[] args) {
+
+//        Realational operator
+/*
+            <,
+            >,
+            <=,
+            >=,
+            ==,
+            !=
+*/
+//        they return  a boolean result
+        System.out.println(1<2);
+        System.out.println(2!=1);
+
+//        Logical operator:
+//        logical operator are used to combine multiple boolean expressions or conditions
+/*
+        && , || ,!
+ */
+
+    }
+}

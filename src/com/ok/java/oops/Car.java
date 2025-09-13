9cdad384d20a20e05694eb1ea521d127ff5cd941
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-09-13 18:39:23 +0530

    Encapsulation

--- src/com/ok/java/oops/Car.java
+++ src/com/ok/java/oops/Car.java
@@ -3,10 +3,26 @@ package com.ok.java.oops;
 import java.lang.invoke.StringConcatFactory;
 
 public class Car {
-    int speed;
-    String color;
+    private int speed;
+   private String color;
+   public Car(String color) //Default constructor
+   {
+        this.color = color;
+   }
 
     void drive(){
         System.out.println("Driving....");
     }
+    public void setSpeed(int speed){
+       this.speed=speed;
+       if (speed<0){
+           System.out.println("Not possible");
+       }
+       else {
+           System.out.println("Driving at:"+speed);
+       }
+
+    }
+
 }
+
--- src/com/ok/java/oops/Test.java
+++ src/com/ok/java/oops/Test.java
@@ -2,9 +2,10 @@ package com.ok.java.oops;
 
 public class Test {
     public static void main(String[] args) {
+//      Object of class car
+        Car car = new Car("Green");
+        car.setSpeed(400);
 
-        Car car = new Car();
-        car.drive();
 
 
     }

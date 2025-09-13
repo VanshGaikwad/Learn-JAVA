e6dc5c74a017712749acdd05d50fd4a60bb3b5e0
vansh gaikwad <vanshgaikwad72@gmail.com>
2025-09-13 17:45:22 +0530

    ternary operator

---
+++ src/com/ok/java/datatypes/ternary_operator.java
@@ -0,0 +1,10 @@
+package com.ok.java.datatypes;
+
+public class ternary_operator {
+    public static void main(String[] args) {
+//          Ternary is shortcut of if else
+        int  a = 2;
+        String isEven = a%2 == 0 ? "Even" :"Odd";
+        System.out.println(isEven);
+    }
+}
